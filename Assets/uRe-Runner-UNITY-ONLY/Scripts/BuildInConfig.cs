1c652f27f42937990672f07d469739576c3641cb
AndyGFX <o.kollar@gmail.com>
2017-09-21 17:14:23 +0200

    Add component for define config internaly as component

---
+++ Assets/uRe-Runner-UNITY-ONLY/Scripts/BuildInConfig.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace uRetroEngine
+{
+    public class BuildInConfig : MonoBehaviour
+    {
+        public string cartridgesFolder = "uRetroEngine_Game";
+        public string cartridgeName = "Game";
+        public TextAsset luaCode;
+        private string fileLua = "<none>";
+        public Texture2D fonts;
+        private string fileFont = "<none>";
+        public Texture2D colors;
+        private string fileColors = "<none>";
+        public Texture2D sprites;
+        private string fileSprites = "<none>";
+        public TextAsset tilemap;
+        private string fileTilemap = "<none>";
+        public string chars = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~ ";
+        public int charSpacing = 8;
+
+        // display resolution
+        public int screen_width = 256;
+
+        public int screen_height = 240;
+        public bool flipScreenY = true;
+
+        // sprite size
+        public int sprite_width = 8;
+
+        public int sprite_height = 8;
+
+        // tilemap size
+        public int tilemap_width = 32;
+
+        public int tilemap_height = 32;
+        public int tilemap_layers = 2;
+
+        // max color in palette
+        public int max_colors = 16;
+
+        // capture to gif
+        public string capture_filename = "capture";
+
+        public int capture_framerate = 15;
+        public int capture_downscale = 2;
+        public int capture_time = 10;
+        public bool capture_bilinear = false;
+        // keys
+
+        public KeyCode UP = KeyCode.UpArrow;
+        public KeyCode DOWN = KeyCode.DownArrow;
+        public KeyCode RIGHT = KeyCode.RightArrow;
+        public KeyCode LEFT = KeyCode.LeftArrow;
+        public KeyCode A = KeyCode.Z;
+        public KeyCode B = KeyCode.X;
+        public KeyCode X = KeyCode.A;
+        public KeyCode Y = KeyCode.S;
+        public KeyCode START = KeyCode.Return;
+        public KeyCode OPTION = KeyCode.Escape;
+
+        public void Initialize()
+        {
+            this.fileLua = this.luaCode.name;
+            this.fileSprites = this.sprites.name;
+            this.fileFont = this.fonts.name;
+            this.fileTilemap = this.tilemap.name;
+            this.fileColors = this.colors.name;
+
+            uRetroConfig.cartridgesFolder = this.cartridgesFolder;
+            uRetroConfig.cartridgeName = this.cartridgeName;
+            uRetroConfig.fileLua = this.fileLua;
+            uRetroConfig.fileFont = this.fileFont;
+            uRetroConfig.fileColors = this.fileColors;
+            uRetroConfig.fileSprites = this.fileSprites;
+            uRetroConfig.fileTilemap = this.fileTilemap;
+            uRetroConfig.chars = this.chars;
+            uRetroConfig.charSpacing = this.charSpacing;
+            uRetroConfig.screen_width = this.screen_width;
+            uRetroConfig.screen_height = this.screen_height;
+            uRetroConfig.flipScreenY = this.flipScreenY;
+            uRetroConfig.sprite_width = this.sprite_width;
+            uRetroConfig.sprite_height = this.sprite_height;
+            uRetroConfig.tilemap_width = this.tilemap_width;
+            uRetroConfig.tilemap_height = this.tilemap_height;
+            uRetroConfig.tilemap_layers = this.tilemap_layers;
+            uRetroConfig.max_colors = this.max_colors;
+            uRetroConfig.capture_filename = this.capture_filename;
+            uRetroConfig.capture_framerate = this.capture_framerate;
+            uRetroConfig.capture_downscale = this.capture_downscale;
+            uRetroConfig.capture_time = this.capture_time;
+            uRetroConfig.capture_bilinear = this.capture_bilinear;
+            uRetroConfig.UP = this.UP;
+            uRetroConfig.DOWN = this.DOWN;
+            uRetroConfig.RIGHT = this.RIGHT;
+            uRetroConfig.LEFT = this.LEFT;
+            uRetroConfig.A = this.A;
+            uRetroConfig.B = this.B;
+            uRetroConfig.X = this.X;
+            uRetroConfig.Y = this.Y;
+            uRetroConfig.START = this.START;
+            uRetroConfig.OPTION = this.OPTION;
+        }
+    }
+}
--- Assets/uRe-Runner-UNITY-ONLY/Scripts/uRetroEngine_Runner_UNITY_ONLY.cs
+++ Assets/uRe-Runner-UNITY-ONLY/Scripts/uRetroEngine_Runner_UNITY_ONLY.cs
@@ -5,7 +5,6 @@ using UnityEngine.UI;
 using System.IO;
 using Newtonsoft.Json;
 
-
 namespace uRetroEngine
 {
     public class uRetroEngine_Runner_UNITY_ONLY : uRetroEngineComponent
@@ -14,8 +13,19 @@ namespace uRetroEngine
         private int yy = 0;
         private int line = 0;
 
+        public string gameFolderName = "uRetroEngine_Game";
+        public string gameName = "DEMO-UnityOnlyGame";
+        public bool useExternalConfig = true;
+        private BuildInConfig config;
+
         private void Awake()
         {
+            if (this.useExternalConfig == false)
+            {
+                this.config = this.GetComponent<BuildInConfig>();
+                if (this.config == null) this.useExternalConfig = true;
+            }
+
             // set targetDisplay for Mouse Input
             uRetroDisplay.displayTarget = GameObject.FindGameObjectWithTag("uRetroDisplay").GetComponent<RectTransform>();
 
@@ -26,46 +36,57 @@ namespace uRetroEngine
             uRetroCapture.Init();
 
             // prepare and set console window
-	        uRetroConsole.console = GameObject.FindGameObjectWithTag("uRetroConsole");
-	        
-	        
-	        uRetroConfig.cartridgeName = "DENO-UnityOnlyGame";
-	        
-	        uRetroConfig.cartridgesFolder = "uRetroEngine_Game";
-	        
-	        string path = uRetroSystem.GetRoot() + "/" + uRetroConfig.cartridgesFolder+"/";
-	        
-            // CONFIG
-	        string json = File.ReadAllText(path + "config.json");
-	        uRetroSystem.DefinitionToConfig(JsonConvert.DeserializeObject<RetroDefinition>(json));
-	        
-            // COLORS
-	        Texture2D colors = PNG.LoadPNG(path + uRetroConfig.fileColors);
-	        uRetroColors.CreatePalette(colors);
-	        
-            // SPRITES
-	        Texture2D sprites = PNG.LoadPNG(path + uRetroConfig.fileSprites);
-	        uRetroSprites.CreateSprites(sprites);
-	        
-            // FONTS
-	        Texture2D fonts = PNG.LoadPNG(path + uRetroConfig.fileFont);
-	        uRetroText.CreateFont(fonts);
-	        
-	        uRetroText.SetFont(0,0,6);
-	        uRetroText.SetFont(1,16*8,6);
-	        
-	        
-            // Tilemaps
-	        uRetroTilemap.Load(path);
-	        
+            uRetroConsole.console = GameObject.FindGameObjectWithTag("uRetroConsole");
+
+            if (useExternalConfig)
+            {
+                uRetroConfig.cartridgeName = this.gameName;
+
+                uRetroConfig.cartridgesFolder = this.gameFolderName;
+
+                string path = uRetroSystem.GetRoot() + "/" + uRetroConfig.cartridgesFolder + "/";
+
+                // CONFIG
+                string json = File.ReadAllText(path + "config.json");
+                uRetroSystem.DefinitionToConfig(JsonConvert.DeserializeObject<RetroDefinition>(json));
+
+                // COLORS
+                Texture2D colors = PNG.LoadPNG(path + uRetroConfig.fileColors);
+                uRetroColors.CreatePalette(colors);
+
+                // SPRITES
+                Texture2D sprites = PNG.LoadPNG(path + uRetroConfig.fileSprites);
+                uRetroSprites.CreateSprites(sprites);
+
+                // FONTS
+                Texture2D fonts = PNG.LoadPNG(path + uRetroConfig.fileFont);
+                uRetroText.CreateFont(fonts);
+
+                uRetroText.SetFont(0, 0, 6);
+                uRetroText.SetFont(1, 16 * 8, 6);
+
+                // Tilemaps
+                uRetroTilemap.Load(path);
+            }
+            else
+            {
+                this.config.Initialize();
+
+                // COLORS
+                uRetroColors.CreatePalette(this.config.colors);
+
+                // SPRITES
+                uRetroSprites.CreateSprites(this.config.sprites);
+
+                // FONTS
+                uRetroText.CreateFont(this.config.fonts);
+            }
+
             // Create Display
-	        uRetroDisplay.CreateDisplay(GameObject.FindGameObjectWithTag("uRetroDisplay"));
-	        
+            uRetroDisplay.CreateDisplay(GameObject.FindGameObjectWithTag("uRetroDisplay"));
+
             // Set Resolution
-	        uRetroDisplay.SetResolution(uRetroConfig.screen_width, uRetroConfig.screen_height, 0);
-	        
-	        
-	        
+            uRetroDisplay.SetResolution(uRetroConfig.screen_width, uRetroConfig.screen_height, 0);
         }
 
         // Use this for initialization
@@ -81,14 +102,14 @@ namespace uRetroEngine
 
             // ---------------------- TEST CODE .....
 
-	        uRetroDisplay.Clear(3);
-	        
-	        uRetroText.Font(0);
-	        uRetroText.Draw(10,18,"2017");
-	        
-	        uRetroText.Font(1);
-	        uRetroText.Draw(10,10,"Standalone unity version");
-	        
+            uRetroDisplay.Clear(3);
+
+            uRetroText.Font(0);
+            uRetroText.Draw(10, 18, "2017");
+
+            uRetroText.Font(1);
+            uRetroText.Draw(10, 10, "Standalone unity version");
+
             uRetroDisplay.Flip();
         }
     }
